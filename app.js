c5fd90e1a461437d21e8f1cdef8f76161216df17
Lizz <lizwesterband@gmail.com>
2020-07-17 14:08:36 -0700

    [app.js] Add 5 variables

---
+++ app.js
@@ -0,0 +1,18 @@
+// 5 variable of different types
+//string
+//number
+//array
+//boolean
+//object
+
+var a = "Hello World";
+var b = 7
+var c = ["cats", "dogs", "dragons"]
+var d = false;
+var e = {
+   name: "Lizz",
+   age: 26,
+   faveThings: ["skating", "coding", "singing"],
+   isAMom: true,
+}
+console.log( a, b, c, d, e);
