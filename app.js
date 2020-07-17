92b77275e9acda7f667274b74d88afeba5212fb1
Lizz <lizwesterband@gmail.com>
2020-07-17 14:37:26 -0700

    [app.js] Add attempt at solo functions

--- app.js
+++ app.js
@@ -16,3 +16,69 @@ var e = {
    isAMom: true,
 }
 console.log( a, b, c, d, e);
+
+function printStuff() {
+    let color = "blue";
+}
+
+//make 10 different functions
+//one to pass in an array
+//one to pass in a callback
+//one to pass in string
+//..ob ject
+//boolean
+
+
+function gimmeLove() {
+    let affection = true;
+}
+function sayMyName() {
+    let name = "Lizz";
+}
+function hollaBack() {
+    let sayHi = gimmeAHoller();
+    let gimmeAHoller = "whats up";
+}
+function theBangers() {
+    let music = ["How far Ill go", "Say my name", "Almost lover"];
+    
+}
+function myHouse() {
+    let house = {
+        color: brown,
+        stories: 1,
+        hasGarage: true,
+
+    };
+}
+function sayKitty() {
+    let phrase = "kitty";
+}
+function isItCold() {
+    let cold = true;
+}
+function holidayList() {
+    let holidays  = {
+        Jan: "New Years",
+        Feb: "Valentines day",
+        March: "St patricks Day",
+        April: "Easter",
+        May: false,
+        June: false,
+        July: "The fourth",
+        August: false,
+        September: "Labor day",
+        October: "Halloween",
+        November: "Thanksgiving",
+        December: ["Christmas", "New Years Eve"]
+
+    };
+}
+function singleOrTaken() {
+    let status = "its complicated";
+}
+function lastOne() {
+    let last = 1;
+
+
+}
